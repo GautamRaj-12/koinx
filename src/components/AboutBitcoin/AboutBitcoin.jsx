cdbcb5d618f6ec9059f7c6cc9dfbd9d38e7b4cfd
Gautam Raj <gautamraj996@gmail.com>
2024-03-05 20:45:34 +0530

    Fix styling and resolve issues in AboutBitcoin component.

--- src/components/AboutBitcoin/AboutBitcoin.jsx
+++ src/components/AboutBitcoin/AboutBitcoin.jsx
@@ -1,13 +1,13 @@
-import profitsPic from "../../assets/profits.png";
-import taxPic from "../../assets/tax-liability.png";
+import profitsPic from '../../assets/profits.png';
+import taxPic from '../../assets/tax-liability.png';
 const AboutBitcoin = () => {
   return (
     <>
-      <section className="p-4 bg-white rounded-sm about-bitcoin md:w-[60%] mx-[5%] mt-4">
-        <h2 className="mb-4 text-2xl font-semibold ">About Bitcoin</h2>
-        <div className="about-bitcoin-top">
-          <h3 className="mb-2 text-lg font-bold">What is Bitcoin?</h3>
-          <p className="pb-2 mb-2 font-semibold border-b-2 text-slate-700">
+      <section className='p-4 bg-white rounded-sm about-bitcoin md:w-[60%] mx-[5%] mt-4'>
+        <h2 className='mb-4 text-2xl font-semibold '>About Bitcoin</h2>
+        <div className='about-bitcoin-top'>
+          <h3 className='mb-2 text-lg font-bold'>What is Bitcoin?</h3>
+          <p className='pb-2 mb-2 font-semibold border-b-2 text-slate-700'>
             Bitcoin's price today is US$16,951.82, with a 24-hour trading volume
             of $19.14 B. BTC is +0.36% in the last 24 hours. It is currently
             -7.70% from its 7-day all-time high of $18,366.66, and 3.40% from
@@ -16,23 +16,23 @@ const AboutBitcoin = () => {
           </p>
         </div>
 
-        <div className="about-bitcoin-middle">
-          <h3 className="mb-2 text-lg font-bold">
+        <div className='about-bitcoin-middle'>
+          <h3 className='mb-2 text-lg font-bold'>
             Lorem ipsum dolor sit amet.
           </h3>
-          <p className="mb-2 font-semibold text-slate-700">
+          <p className='mb-2 font-semibold text-slate-700'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             nihil quas commodi animi dignissimos cupiditate, doloremque,
             sapiente aspernatur quibusdam veniam iusto eveniet repudiandae
             dolores aperiam aut reiciendis hic? Excepturi, molestias?
           </p>
-          <p className="mb-2 font-semibold text-slate-700">
+          <p className='mb-2 font-semibold text-slate-700'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
             officiis, quisquam neque asperiores qui quasi voluptas voluptatum
             dolor rem. Earum repellendus, nisi odio ducimus commodi ullam sequi
             illo architecto fugit?
           </p>
-          <p className="pb-2 mb-2 font-semibold border-b-2 text-slate-700">
+          <p className='pb-2 mb-2 font-semibold border-b-2 text-slate-700'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Reprehenderit voluptatem aspernatur possimus dolorum aliquid.
             Possimus rerum, quaerat aliquid nesciunt optio magni veritatis
@@ -40,45 +40,45 @@ const AboutBitcoin = () => {
           </p>
         </div>
 
-        <div className="about-bitcoin-bottom">
-          <h3 className="mb-4 text-2xl font-semibold">
+        <div className='about-bitcoin-bottom'>
+          <h3 className='mb-4 text-2xl font-semibold'>
             Already Holding Bitcoin?
           </h3>
-          <div className="grid grid-cols-1 gap-6 pb-2 mb-2 border-b-2 h-50 lg:grid-cols-2">
-            <div className="flex h-40 gap-4 rounded-lg bg-gradient-to-br from-green-600 to-blue-500">
+          <div className='grid grid-cols-1 gap-6 pb-2 mb-2 border-b-2 h-50 lg:grid-cols-2'>
+            <div className='flex h-40 gap-4 rounded-lg bg-gradient-to-br from-green-600 to-blue-500'>
               <img
-                src={profitsPic}
-                alt=""
-                className="w-[35%] object-cover m-2 rounded-lg"
+                src={taxPic}
+                alt=''
+                className='w-[35%] object-cover m-2 rounded-lg'
               />
-              <div className="flex flex-col gap-4 w-[35%] justify-start mt-4 items-start">
-                <p className="text-xl font-semibold text-white">
-                  Check your profits
+              <div className='flex flex-col gap-4 w-[38%] justify-start mt-4 items-start'>
+                <p className='text-xl font-semibold text-white'>
+                  Calculate your Profits
                 </p>
-                <button className="flex items-center gap-2 px-4 py-1 font-semibold bg-white rounded-lg">
-                  <span>Check Now </span>{" "}
-                  <i className="mt-[3px] fa-solid fa-arrow-right"></i>
+                <button className='flex items-center gap-2 px-4 py-1 font-semibold bg-white rounded-lg'>
+                  <span>Check Now </span>
+                  <i className='mt-[3px] fa-solid fa-arrow-right'></i>
                 </button>
               </div>
             </div>
-            <div className="flex h-40 gap-4 rounded-lg bg-gradient-to-br from-orange-400 to-orange-700">
+            <div className='flex h-40 gap-4 rounded-lg bg-gradient-to-br from-orange-400 to-orange-700'>
               <img
                 src={taxPic}
-                alt=""
-                className="w-[35%] object-cover m-2 rounded-lg"
+                alt=''
+                className='w-[35%] object-cover m-2 rounded-lg'
               />
-              <div className="flex flex-col gap-4 w-[38%] justify-start mt-4 items-start">
-                <p className="text-xl font-semibold text-white">
+              <div className='flex flex-col gap-4 w-[38%] justify-start mt-4 items-start'>
+                <p className='text-xl font-semibold text-white'>
                   Calculate your tax liability
                 </p>
-                <button className="flex items-center gap-2 px-4 py-1 font-semibold bg-white rounded-lg">
-                  <span>Check Now </span>{" "}
-                  <i className="mt-[3px] fa-solid fa-arrow-right"></i>
+                <button className='flex items-center gap-2 px-4 py-1 font-semibold bg-white rounded-lg'>
+                  <span>Check Now </span>
+                  <i className='mt-[3px] fa-solid fa-arrow-right'></i>
                 </button>
               </div>
             </div>
           </div>
-          <p className="mb-2 font-semibold text-slate-700">
+          <p className='mb-2 font-semibold text-slate-700'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Reprehenderit voluptatem aspernatur possimus dolorum aliquid.
             Possimus rerum, quaerat aliquid nesciunt optio magni veritatis
