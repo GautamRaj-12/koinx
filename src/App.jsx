ed7c00d3fd0c49c2028fac2f9d2359453e9f8abc
Gautam Raj <gautamraj996@gmail.com>
2024-03-05 16:09:43 +0530

    Add About Component Desktop Version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import AboutBitcoin from './components/AboutBitcoin/AboutBitcoin'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <AboutBitcoin/>
     </>
   )
 }
