38f9e761b7dcbd1be87d67a99e1431999ca07504
Gautam Raj <gautamraj996@gmail.com>
2024-03-06 06:29:52 +0530

    Fix bg color of header in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
-import koinxLogo from "../../assets/koinx-logo.svg"
-const Navbar = () =>{
-    return(
-        <>
-        <header className="shadow-lg">
-            <nav className="flex w-[90%] mx-auto justify-between items-center p-3">
-                <div><img src={koinxLogo} alt="" className="w-40"/></div>
-                <ul className="flex gap-16">
-                    <li className="py-1 font-semibold">Crypto Taxes</li>
-                    <li className="py-1 font-semibold">Free Tools</li>
-                    <li className="py-1 font-semibold">Resource Center</li>
-                    <li className="px-4 py-1 font-semibold bg-blue-600 rounded-md text-slate-100">Get Started</li>
-                </ul>
-            </nav>
-        </header>
-        </>
-    )
-}
+import koinxLogo from '../../assets/koinx-logo.svg';
+const Navbar = () => {
+  return (
+    <>
+      <header className='bg-white shadow-lg'>
+        <nav className='flex w-[90%] mx-auto justify-between items-center p-3'>
+          <div>
+            <img src={koinxLogo} alt='' className='w-40' />
+          </div>
+          <ul className='flex gap-16'>
+            <li className='py-1 font-semibold'>Crypto Taxes</li>
+            <li className='py-1 font-semibold'>Free Tools</li>
+            <li className='py-1 font-semibold'>Resource Center</li>
+            <li className='px-4 py-1 font-semibold bg-blue-600 rounded-md text-slate-100'>
+              Get Started
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 export default Navbar;
