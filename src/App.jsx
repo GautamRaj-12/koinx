24c1fd59ee48cdfeecb5f37fef688195136d53e6
Gautam Raj <gautamraj996@gmail.com>
2024-03-06 09:06:35 +0530

    Add chart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import AboutBitcoin from './components/AboutBitcoin/AboutBitcoin';
+import Chart from './components/Chart/Chart';
 import Navbar from './components/Navbar/Navbar';
 import Team from './components/Team/Team';
 import Tokenomics from './components/Tokenomics/Tokenomics';
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Chart />
       <AboutBitcoin />
       <Tokenomics />
       <Team />
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,56 @@
+// TradingViewWidget.jsx
+import React, { useEffect, useRef, memo } from 'react';
+
+const Chart = () => {
+  const containerRef = useRef();
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = `
+        {
+          "width": "700",
+          "height": "500",
+          "symbol": "BITSTAMP:BTCUSD",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "light",
+          "style": "2",
+          "locale": "en",
+          "enable_publishing": false,
+          "calendar": false,
+          "hide_volume":true,
+          "support_host": "https://www.tradingview.com"
+        }`;
+    containerRef.current.appendChild(script);
+
+    // Cleanup the script when the component is unmounted
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeChild(script);
+      }
+    };
+  }, []);
+
+  return (
+    <section className='p-4 bg-white rounded-sm md:w-[60%] mx-[5%] mt-4'>
+      <div className='tradingview-widget-container' ref={containerRef}>
+        <div className='tradingview-widget-container__widget'></div>
+        <div className='tradingview-widget-copyright'>
+          <a
+            href='https://www.tradingview.com/'
+            rel='noopener nofollow'
+            target='_blank'
+          >
+            <span className='blue-text'>Track all markets on TradingView</span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default memo(Chart);
