08f3b5223d21289335c731c86e9bcccc83f2f198
Gautam Raj <gautamraj996@gmail.com>
2024-03-07 06:34:48 +0530

    Fix carousel navigation issue

--- src/components/YouMayLike/YouMayLike.jsx
+++ src/components/YouMayLike/YouMayLike.jsx
@@ -1,5 +1,6 @@
 const YouMayLike = ({ allTrendingCoin }) => {
-  let cardContainer = document.querySelector(".card-container");
+  let cardContainer = document.querySelector('.card-container');
+  let trendCardContainer = document.querySelector('.trend-card-container');
   const btnPressPrev = () => {
     let width = cardContainer.clientWidth;
     cardContainer.scrollLeft = cardContainer.scrollLeft - width;
@@ -10,40 +11,88 @@ const YouMayLike = ({ allTrendingCoin }) => {
     cardContainer.scrollLeft = cardContainer.scrollLeft + width;
     console.log(width);
   };
+
+  const trendBtnPressPrev = () => {
+    let width = trendCardContainer.clientWidth;
+    trendCardContainer.scrollLeft = trendCardContainer.scrollLeft - width;
+    console.log(width);
+  };
+  const trendBtnPressNext = () => {
+    let width = trendCardContainer.clientWidth;
+    trendCardContainer.scrollLeft = trendCardContainer.scrollLeft + width;
+    console.log(width);
+  };
   return (
     <>
-      <section className="mt-4 bg-white">
-        <h2 className="mx-auto w-[90%] mb-4 text-2xl font-semibold py-4 ">
+      <section className='mt-4 bg-white relative'>
+        <h2 className='mx-auto w-[90%] mb-4 text-2xl font-semibold py-4 '>
           You May Also Like
         </h2>
-        <div className="flex gap-4 mx-auto w-[90%] relative card-container scroll-smooth overflow-hidden">
+        <div className='flex gap-4 mx-auto w-[90%] card-container scroll-smooth overflow-hidden'>
           {allTrendingCoin.map((coin) => (
             <div key={coin.symbol}>
-              <div className="flex flex-col w-48 gap-2 p-2 border rounded-md">
-                <div className="flex items-center gap-2">
-                  <img src={coin.thumb} alt="" className="w-6 h-6" />
-                  <p className="font-semibold">{coin.symbol}</p>
-                  <p className="font-semibold">{coin.usd.toFixed(2)}</p>
+              <div className='flex flex-col w-48 gap-2 p-2 border rounded-md'>
+                <div className='flex items-center gap-2'>
+                  <img src={coin.thumb} alt='' className='w-6 h-6' />
+                  <p className='font-semibold'>{coin.symbol}</p>
+                  <p className='font-semibold'>{coin.usd.toFixed(2)}</p>
                 </div>
                 <div>
-                  <p className="text-xl font-semibold">{coin.price}</p>
+                  <p className='text-xl font-semibold'>{coin.price}</p>
                 </div>
                 <div>
-                  <img src={coin.sparkline} alt="" />
+                  <img src={coin.sparkline} alt='' />
                 </div>
               </div>
             </div>
           ))}
-          <div className="absolute flex items-center justify-center w-8 h-[100%] bg-white rounded-full">
+          <div className='absolute flex items-center justify-center w-8 h-8 bg-white rounded-full top-32 left-12'>
             <i
-              className="cursor-pointer fa-solid fa-angle-left"
+              className='cursor-pointer fa-solid fa-angle-left'
               onClick={btnPressPrev}
             ></i>
           </div>
-          <div className="absolute flex items-center justify-center w-8 h-[100%] bg-white rounded-full right-0">
+          <div className='absolute flex items-center justify-center w-8 h-8 bg-white rounded-full right-10 top-32'>
             <i
-              className="cursor-pointer fa-solid fa-angle-right"
+              className='cursor-pointer fa-solid fa-angle-right'
               onClick={btnPressNext}
+            ></i>
+          </div>
+        </div>
+      </section>
+
+      <section className='bg-white relative'>
+        <h2 className='mx-auto w-[90%] mb-4 text-2xl font-semibold py-4 '>
+          Trending Coins
+        </h2>
+        <div className='flex gap-4 mx-auto w-[90%] trend-card-container scroll-smooth overflow-hidden'>
+          {allTrendingCoin.map((coin) => (
+            <div key={coin.symbol}>
+              <div className='flex flex-col w-48 gap-2 p-2 border rounded-md'>
+                <div className='flex items-center gap-2'>
+                  <img src={coin.thumb} alt='' className='w-6 h-6' />
+                  <p className='font-semibold'>{coin.symbol}</p>
+                  <p className='font-semibold'>{coin.usd.toFixed(2)}</p>
+                </div>
+                <div>
+                  <p className='text-xl font-semibold'>{coin.price}</p>
+                </div>
+                <div>
+                  <img src={coin.sparkline} alt='' />
+                </div>
+              </div>
+            </div>
+          ))}
+          <div className='absolute flex items-center justify-center w-8 h-8 bg-white rounded-full top-32 left-12'>
+            <i
+              className='cursor-pointer fa-solid fa-angle-left'
+              onClick={trendBtnPressPrev}
+            ></i>
+          </div>
+          <div className='absolute flex items-center justify-center w-8 h-8 bg-white rounded-full right-10 top-32'>
+            <i
+              className='cursor-pointer fa-solid fa-angle-right'
+              onClick={trendBtnPressNext}
             ></i>
           </div>
         </div>
